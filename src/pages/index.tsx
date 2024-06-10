eebc13e8acea5af3580ff0f88a82b020a24109a9
yhwabillie <yhwabillie93@gmail.com>
2024-06-10 13:53:52 +0900

    tz 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Page({ data }: PageProps<Queries.WorksQuery>) {
               <li key={index}>
                 <Link to={`/work-experience/${item.id}`}>
                   <strong>{item.title}</strong>
-                  <p>{`게시일: ${dayjs(item.createdAt).format('YYYY-MM-DD a hh:mm:ss')}`}</p>
+                  <p>{`게시일: ${dayjs(item.createdAt).tz().format('YYYY-MM-DD a hh:mm:ss')}`}</p>
                   <p>{`업데이트: ${dayjs(item.updatedAt).fromNow()}`}</p>
                 </Link>
               </li>
