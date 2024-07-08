807149cc221d6192c4bc8f56a11676c9e91aeb89
yhwabillie <yhwabillie93@gmail.com>
2024-07-08 10:26:29 +0900

    비디오 code split 다시 시도

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -209,7 +209,7 @@ export default function Page({ data }: PageProps<Queries.MainPageQuery>) {
               <span className="sr-only">포트폴리오 영상 화면 영역</span>
               <Video />
 
-              {/* <Suspense
+              <Suspense
                 fallback={
                   <StaticImage
                     src="../images/videos/summary_thumbnail.png"
@@ -221,7 +221,7 @@ export default function Page({ data }: PageProps<Queries.MainPageQuery>) {
                 }
               >
                 <Video />
-              </Suspense> */}
+              </Suspense>
             </div>
             <div className="absolute top-0 bottom-0 left-0 right-[26%] summary-title-bg">
               <span className="sr-only">영상 화면 영역 타이틀을 위한 음영 배경</span>
